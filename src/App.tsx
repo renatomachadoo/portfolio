78122bb9f6c75b5fd50222e2fcd53802c92f6572
Renato Machado <renatomachado176@gmail.com>
2025-07-09 21:37:02 +0100

    feat: added header and nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
 export function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="min-h-screen px-8 py-4">
+      <div className="container mx-auto h-full">
+        <header className="mx-auto flex items-center justify-center rounded-md bg-zinc-900 px-8 py-4">
+          {/* <a className="hover:underline" href="#hero">
+            Renato Machado
+          </a> */}
+          <nav className="flex items-center justify-center gap-8">
+            <a className="uppercase hover:underline" href="#about-me">
+              ABOUT-ME
+            </a>
+            <a className="uppercase hover:underline" href="#skills">
+              SKILLS
+            </a>
+            <a className="uppercase hover:underline" href="#projects">
+              PROJECTS
+            </a>
+          </nav>
+        </header>
+      </div>
+    </div>
+  );
 }
