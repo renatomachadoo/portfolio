52e24a255ecf22b6d5e8b94797172410a563d399
Renato Machado <renatomachado176@gmail.com>
2025-07-11 19:17:56 +0100

    Revert "fix: fixed header and footer issue not applying correcly w-full on safari browser"
    This reverts commit 088269239e8472b04834babf20593e033c88a748.

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import { Tooltip } from './components/ui/Tooltip';
 
 export function App() {
   return (
-    <div className="min-h-screen w-full">
+    <div className="min-h-screen">
       <Header />
       <main className="w-full px-8">
         <Section id="about" title="About">
